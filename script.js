5595e5f909408989f55e241b505710e816c271c0
Stuti Kaushik <kaushikstuti4@gmail.com>
2024-09-10 14:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
-  // your code here
+    if (s === '' || !s.includes(' ')) {
+        return s;
+    } else {
+        return s.substring(0, s.indexOf(' '));
+    }
 }
 
-// Do not change the code below
+console.log(firstWord('see and stop')); // Output: 'see'
+console.log(firstWord(' Hello World!')); // Output: 'Hello'
+console.log(firstWord('12345')); // Output: '12345'
+console.log(firstWord('')); // Output: ''
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
